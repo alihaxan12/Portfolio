b424e78625e52b98039d76b9e340989876788a85
Ali Hassan <alihaxan514@gmail.com>
2022-11-26 15:23:05 +0500

    first commit

--- pages/Business.jsx
+++ pages/Business.jsx
@@ -7,7 +7,7 @@ import { BsFillRecordCircleFill, BsRecord2, BsRecord2Fill } from 'react-icons/bs
     <div id='business' className='w-full'>
         <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
             <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
-                <Image className='absolute z-1' layout='fill' objectFit='cover' src='/images/myapp.JPG' alt='/'  />
+                <Image className='absolute z-1' layout='fill' objectFit='cover' src='/images/myapp.JPG' alt='website'  />
                 <div className='absolute  top-[70%] max-w-[1240px] w-full left-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
                 <h1 className='text-3xl font-bold text-white'>Food Business</h1>
                 <p className='text-xl '>React JS / Tailwind </p>
